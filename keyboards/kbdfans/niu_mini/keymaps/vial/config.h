7b61fd1000329bff22480d228f987e9139655eb8
Nisker <Benjamin-nisker@hotmail.com>
2022-09-25 20:20:57 +0200

    Vial support for niu mini

---
+++ keyboards/kbdfans/niu_mini/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+#define VIAL_KEYBOARD_UID {0xB2, 0x24, 0x9A, 0xBE, 0x85, 0x34, 0x35, 0xAF}
+
+#undef RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
